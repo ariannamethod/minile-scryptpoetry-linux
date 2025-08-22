9ab11ea83d6c3284a4f289cc06391fb1dc7f0391
Arianna Method <theariannamethod@gmail.com>
2025-08-22 21:02:36 +0300

    Initialize bio module with components and aggregator

---
+++ arianna_core/bio/__init__.py
@@ -0,0 +1,13 @@
+"""Biological state components and aggregator."""
+
+from .cell_resonance import CellResonance
+from .pain_marker import PainMarker
+from .love_field import LoveField
+from .orchestra import BioOrchestra
+
+__all__ = [
+    "CellResonance",
+    "PainMarker",
+    "LoveField",
+    "BioOrchestra",
+]
